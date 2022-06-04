58d074cb61394b200b0965799989b044afded7d6
Astha Sharma <104688460+Astha381513@users.noreply.github.com>
2022-06-04 01:40:17 -0700

    Create even odd function.c

---
+++ FUNCTION PROGRAMMS/even odd function.c
@@ -0,0 +1,17 @@
+#include<stdioh>
+int checkoddeven(int n);
+int main()
+{
+    int n;
+printf("enter the value of n : ");
+scanf("%d", &n);
+if check odd even()
+printf("even number");
+else
+printf("odd number");
+return 0;
+}
+int checkoddeven(int n);
+{
+    n = %2;
+    printf("even number");
