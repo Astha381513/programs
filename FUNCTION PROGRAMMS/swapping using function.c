ea733d52483a53c3b20053ecad464e80f091dc8d
Astha Sharma <104688460+Astha381513@users.noreply.github.com>
2022-06-04 01:39:08 -0700

    Create swapping using function.c

---
+++ FUNCTION PROGRAMMS/swapping using function.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+double square(double num);
+int main()
+    {
+        int num;
+       double n;
+       printf("enter the value of square : ");
+       scanf("%d",&num);
+       n = square(num);
+       printf("the square is %d : %2f ",n);
+       return 0;
+    }
+double square(double num)
+{
+    return  (num * num);
+}
