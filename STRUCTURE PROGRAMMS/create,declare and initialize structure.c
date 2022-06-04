4487feb7ea89bf64cce3bfac399a137a949c22ed
Astha Sharma <104688460+Astha381513@users.noreply.github.com>
2022-06-04 07:59:26 -0700

    Create create,declare and initialize structure.c

---
+++ STRUCTURE PROGRAMMS/create,declare and initialize structure.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+struct student
+{
+int id;
+int marks;
+char fav_char;
+
+};
+
+int main()
+{
+    struct student disha;
+    disha.id = 12;
+    disha.marks = 66;
+    disha.fav_char = 'p';
+    printf("disha got %d marks\n",disha.marks);
+        printf("disha's %d id\n",disha.id);
+                printf("disha's %d fav_char\n",disha.fav_char);
+
+
+    return 0;
+}
